a492aacb4cebb3a432165e264354e12f5c2c0a53
YaSanyaBeats <alexandr191261@yandex.ru>
2023-06-20 00:12:20 +0700

    Add animation on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,22 @@
-/**
- * !(i)
- * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
- * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый (не вызванный) код в итоговый файл не попадает.
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
- * Если мы хотим добавить модуль следует его раскомментировать
- */
-import {
-  isWebp,
-  headerFixed,
-  togglePopupWindows,
-  addTouchClass,
-  addLoadedClass,
-} from './modules';
+gsap.registerPlugin(ScrollTrigger);
 
-// import BurgerMenu from './modules/BurgerMenu';
+const timeline = gsap.timeline();
+timeline.fromTo('.about', {x: '-100vw', y: '+100vh'}, {y: '0'});
+timeline.fromTo('.categoryes', {x: '-200vw', y: '+100vh'}, {y: '0'});
 
-// import Tabs from 'modules/Tabs';
+const main = document.querySelector('main');
 
-// import { MousePRLX } from './libs/parallaxMouse'
-
-// import AOS from 'aos'
-
-// import Swiper, { Navigation, Pagination } from 'swiper';
-
-/* Проверка поддержки webp, добавление класса webp или no-webp для HTML
- ! (i) необходимо для корректного отображения webp из css
- */
-isWebp();
-
-/* Добавление класса touch для HTML если браузер мобильный */
-// addTouchClass();
-
-/* Добавление loaded для HTML после полной загрузки страницы */
-// addLoadedClass();
-
-/* Модуль для работы с меню (Бургер) */
-// new BurgerMenu().init();
-
-/**
- *  Библиотека для анимаций
- *  документация: https://michalsnik.github.io/aos
- */
-// AOS.init();
-
-/** Параллакс мышей */
-// const mousePrlx = new MousePRLX({});
-
-/** Фиксированный header */
-// headerFixed();
-
-/**
- *  Открытие/закрытие модальных окон
- * Чтобы модальное окно открывалось и закрывалось
- * На окно повешай атрибут data-popup="<название окна>"
- * И на кнопку, которая вызывает окно так же повешай атрибут data-type="<название окна>"
-
- * На обертку(враппер) окна добавь класс _overlay-bg
- * На кнопку для закрытия окна добавь класс button-close
- */
-// togglePopupWindows();
-
-// const tabs = new Tabs('default-tabs', {});
+ScrollTrigger.create({
+  animation: timeline,
+  trigger: '.anim',
+  start: 'top top',
+  end: () => main.offsetWidth / 1.2,
+  scrub: true,
+  pin: true
+})
 
 const scrollLink = document.getElementById('scroll-link');
 const scrollElement = document.getElementById('scroll-element');

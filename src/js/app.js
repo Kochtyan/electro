f86a4b71462557f4f64bbc7a13cc3155c5912665
alexa <alexandr191261@yandex.ru>
2023-06-20 15:52:05 +0700

    final desktop main

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,24 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const timeline = gsap.timeline();
-timeline.fromTo('.about', {x: '-100vw', y: '+100vh'}, {y: '0'});
-timeline.fromTo('.categoryes', {x: '-200vw', y: '+100vh'}, {y: '0'});
+timeline.fromTo('.about', {x: '-100vw', y: '100vh'}, {y: '0'});
+timeline.from('.about__image', {scale: 0.5, opacity: 0, rotate: 10}, '<0.2');
+timeline.fromTo('.category', {x: '-200vw', y: '100vh'}, {y: '0'});
+timeline.from('#category-card__image1', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
+timeline.fromTo('.category', {x: '-200vw', y: '0', backgroundColor: 'white'}, {y: '-100vh', backgroundColor: 'black'}, '>0.2');
+timeline.from('#category-card__image2', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
+timeline.fromTo('.info', {x: '-300vw', y: '100vh'}, {y: '0'});
+timeline.to('.category', {y: '-140vh'}, '<0.1');
 
-const main = document.querySelector('main');
+
+const animBlock = document.querySelector('.anim');
+console.log(animBlock.offsetWidth);
 
 ScrollTrigger.create({
   animation: timeline,
   trigger: '.anim',
   start: 'top top',
-  end: () => main.offsetWidth / 1.2,
+  end: 6000,
   scrub: true,
   pin: true
 })

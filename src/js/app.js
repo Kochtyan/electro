e1bbb19e4c7b162b782d20730155aab3c5d166bb
alexa <alexandr191261@yandex.ru>
2023-06-26 17:21:22 +0700

    Adaptive pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,89 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+function setMainAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
 
-const timeline = gsap.timeline();
-timeline.fromTo('.about', {x: '-100vw', y: '100vh'}, {y: '0'});
-timeline.from('.about__image', {scale: 0.5, opacity: 0, rotate: 10}, '<0.2');
-timeline.fromTo('.category', {x: '-200vw', y: '100vh'}, {y: '0'});
-timeline.from('#category-card__image1', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
-timeline.fromTo('.category', {x: '-200vw', y: '0', backgroundColor: 'white'}, {y: '-100vh', backgroundColor: 'black'}, '>0.2');
-timeline.from('#category-card__image2', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
-timeline.fromTo('.info', {x: '-300vw', y: '100vh'}, {y: '0'});
-timeline.to('.category', {y: '-140vh'}, '<0.1');
+    const timeline = gsap.timeline();
+    timeline.fromTo('.about', {x: '-100vw', y: '100vh'}, {y: '0'});
+    timeline.from('.about__image', {scale: 0.5, opacity: 0, rotate: 10}, '<0.2');
+    timeline.fromTo('.category', {x: '-200vw', y: '100vh'}, {y: '0'});
+    timeline.from('#category-card__image1', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
+    timeline.fromTo('.category', {x: '-200vw', y: '0', backgroundColor: 'white'}, {y: '-100vh', backgroundColor: 'black'}, '>0.2');
+    timeline.from('#category-card__image2', {scale: 0.5, opacity: 0, y: -100}, '<0.2');
+    timeline.fromTo('.info', {x: '-300vw', y: '100vh'}, {y: '0'});
+    timeline.to('.category', {y: '-140vh'}, '<0.1');
 
-const animBlock = document.querySelector('.anim');
-console.log(animBlock.offsetWidth);
+    const animBlock = document.querySelector('.anim');
+    console.log(animBlock.offsetWidth);
 
-ScrollTrigger.create({
-  animation: timeline,
-  trigger: '.anim',
-  start: 'top top',
-  end: 6000,
-  scrub: true,
-  pin: true
+    ScrollTrigger.create({
+        animation: timeline,
+        trigger: '.anim',
+        start: 'top top',
+        end: 6000,
+        scrub: true,
+        pin: true
+    })
+}
+
+class Tabs {
+    constructor(root) {
+        this.root = root;
+        this.buttons = this.root.querySelectorAll('.tabs__button');
+        this.panes = this.root.querySelectorAll('.tabs__pane');
+        this.activeIndex = 0;
+
+        if(this.buttons.length != this.panes.length) {
+            console.error('Length of buttons and panes not equal (class Tabs)');
+            return;
+        }
+
+        this.bindListeners();
+    }
+
+    bindListeners() {
+        this.buttons.forEach((button, index) => {
+            button.addEventListener('click', (event) => {
+                this.switchPane(index);
+            })
+        })
+    }
+
+    switchPane(index) {
+        this.panes[this.activeIndex].classList.remove('tabs__pane_show');
+        this.buttons[this.activeIndex].classList.remove('button_accent');
+        this.activeIndex = index;
+        this.panes[this.activeIndex].classList.add('tabs__pane_show');
+        this.buttons[this.activeIndex].classList.add('button_accent');
+    }
+}
+
+function initTabs() {
+    const tabs = document.querySelectorAll('.tabs');
+    if(tabs.length > 0) {
+        tabs.forEach((tab) => {
+            new Tabs(tab);
+        })
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const isTablet = document.documentElement.clientWidth < 1100;
+    const isMobile = document.documentElement.clientWidth < 768;
+
+    if(!isTablet) {
+        const animBlock = document.querySelector('.anim');
+        if(animBlock) {
+            setMainAnimation();
+        }
+    }
+
+    initTabs();
 })
+
+
 
 
 
